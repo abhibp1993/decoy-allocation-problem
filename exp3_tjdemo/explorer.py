163f3655e6b787e8441025c2af7ea32f192bdad6
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2023-11-08 10:57:20 -0500

    #wip: Tiles + their selection logic implemented

--- exp3_tjdemo/explorer.py
+++ exp3_tjdemo/explorer.py
@@ -1,120 +1,82 @@
 import functools
-from mywidgets import *
+import sys
+
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
+    QPushButton,
+    QGridLayout,
+    QTextEdit,
+    QLabel,
+    QFileDialog,
+    QDockWidget
+)
+from mywidgets import ImageButton
 
 __author__ = "Abhishek N. Kulkarni"
 
 
 class TJDecoyAllocExplorer(QMainWindow):
-    def __init__(self, tsys, gw_params):
-        """
-        :param tsys: (dict) Serialized game on graph model.
-        :param gw_params: (dict) Parameters of the gridworld game.
-        """
-        super(TJDecoyAllocExplorer, self).__init__()
+    def __init__(self):
+        super().__init__()
 
-        # Extract parameters
-        self.rows = gw_params["rows"]
-        self.cols = gw_params["cols"]
-        self.obstacles = gw_params["obstacles"]
-        self.real_cheese = gw_params["real_cheese"]
+        # Game parameters
 
-        # Properties
-        self._selected_control = None
+        # Simulator properties
+        self._selected_tile = None
 
         # Set up the window
-        self.window_title = "Tom & Jerry Decoy Allocation Explorer"
-        self.setWindowTitle(self.window_title)
+        # self.setMinimumSize(800, 600)
+        self.setWindowTitle("Tom & Jerry Decoy Allocation Explorer")
 
         # Set up the main widget
         self._main_layout = QVBoxLayout()
-
-        # Add place buttons
-        self._controls_layout = QHBoxLayout()
-        self._controls_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self._controls = [
-            ImageButton(name="place_tom", impath="tom.png", parent=self),
-            ImageButton(name="place_jerry", impath="jerry.png", parent=self),
-            ImageButton(name="place_fake", impath="fake.jpg", parent=self),
-            ImageButton(name="place_trap", impath="trap.jpg", parent=self)
-        ]
-
-        for control in self._controls:
-            control.setFixedWidth(40)
-            control.setFixedHeight(40)
-            control.mousePressEvent = functools.partial(self._control_select_changed, control=control)
-            self._controls_layout.addWidget(control)
-
-        self._main_layout.addLayout(self._controls_layout)
-
-        # Add gridworld
-        self._gridworld = Gridworld(
-            rows=self.rows,
-            cols=self.cols,
-            obstacles=self.obstacles,
-            real_cheese=self.real_cheese
-        )
-        for cell in self._gridworld.cells():
-            cell.mousePressEvent = functools.partial(self._cell_clicked, cell=cell)
-
-        self._main_layout.addWidget(self._gridworld)
 
         # Set central widget
         central_widget = QWidget(self)
         central_widget.setLayout(self._main_layout)
         self.setCentralWidget(central_widget)
 
-    def _control_select_changed(self, e, control):
-        if control.is_selected():
-            control.set_selected(False)
-            self._selected_control = None
+        # Add object buttons
+        self._tiles_layout = QHBoxLayout()
+        self._tiles_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self._tiles = [
+            ImageButton(name="place_tom", impath="tom.png", parent=self),
+            ImageButton(name="place_jerry", impath="jerry.png", parent=self),
+            ImageButton(name="place_fake", impath="fake.jpg", parent=self),
+            ImageButton(name="place_trap", impath="trap.jpg", parent=self)
+        ]
+
+        for control in self._tiles:
+            control.setFixedWidth(40)
+            control.setFixedHeight(40)
+            control.mousePressEvent = functools.partial(self.tile_select_changed, tile=control)
+            self._tiles_layout.addWidget(control)
+
+        self._tiles[0].setEnabled(False)
+        self._tiles[1].setEnabled(False)
+        self._main_layout.addLayout(self._tiles_layout)
+
+    def tile_select_changed(self, e, tile):
+        if tile.is_selected():
+            tile.set_selected(False)
+            self._selected_tile = None
         else:
-            control.set_selected(True)
-            self._selected_control = control
-            for other_control in self._controls:
-                if other_control != control:
-                    other_control.set_selected(False)
+            tile.set_selected(True)
+            self._selected_tile = tile
+            for other_tile in self._tiles:
+                if other_tile != tile:
+                    other_tile.set_selected(False)
 
-    def _cell_clicked(self, e, cell):
-        print("placing in ", cell)
-        if self._selected_control is not None and self._selected_control.name() == "place_fake":
-            # If fake target exists in cell, remove it. Else, add it.
-            if cell.has_widget(f"fake@{cell.name()}"):
-                cell.remove_widget(f"fake@{cell.name()}")
-            else:
-                fake = ImageButton(f"fake@{cell.name()}", "fake.jpg", parent=cell)
-                fake.mousePressEvent = functools.partial(self._cell_clicked, cell=cell)
-                fake.setFixedSize(40, 40)
-                cell.add_widget(fake)
-
-        elif self._selected_control is not None and self._selected_control.name() == "place_trap":
-            # If fake target exists in cell, remove it. Else, add it.
-            if cell.has_widget(f"trap@{cell.name()}"):
-                cell.remove_widget(f"trap@{cell.name()}")
-            else:
-                trap = ImageButton(f"trap@{cell.name()}", "trap.jpg", parent=cell)
-                trap.mousePressEvent = functools.partial(self._cell_clicked, cell=cell)
-                # fake.setFixedSize(40, 40)
-                cell.add_widget(trap)
-
-
-def main():
-    # Load model
-    gw_params = {
-        "rows": 5,
-        "cols": 5,
-        "obstacles": [(1, 1), (2, 1), (3, 1), (4, 1)],
-        "real_cheese": [(1, 4), (4, 4)]
-    }
-
-    # Instantiate gridworld game
-    app = QApplication(sys.argv)
-    explorer = TJDecoyAllocExplorer(
-        tsys=None,
-        gw_params=gw_params
-    )
-    explorer.show()
-    sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = TJDecoyAllocExplorer()
+    ex.show()
+    sys.exit(app.exec())
